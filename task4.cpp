3f70b8273fd2d0cf35a90a6d9ed9a5fdf0ed4370
1ARZAMAS <lizaakutkina@gmail.com>
2024-05-19 21:07:36 +0700

    added 4th task

---
+++ task4.cpp
@@ -0,0 +1,23 @@
+/*Необходимо реверсировать слова в строке, не изменяя порядок слов.*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    system("chcp 65001");
+    string sentence = "Annushka has already bought sunflower oil, and not only bought it, but even spilled it";
+    istringstream iss(sentence);
+    string word;
+    string result;
+
+    while (iss >> word) {
+        reverse(word.begin(), word.end());
+        result += word + " ";
+    }
+
+    cout << "Реверсированные слова: " << result;
+}
